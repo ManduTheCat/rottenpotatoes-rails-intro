af70192d1d747889a31a44fc834c908b2b56de65
EC2 Default User <ec2-user@ip-172-31-92-212.ec2.internal>
2019-12-21 21:16:22 +0000

    finsh hw3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,62 @@
+# class MoviesController < ApplicationController
+
+#   def movie_params
+#     params.require(:movie).permit(:title, :rating, :description, :release_date)
+#   end
+
+#   def show
+#     id = params[:id] # retrieve movie ID from URI route
+#     @movie = Movie.find(id) # look up movie by unique ID
+#     # will render app/views/movies/show.<extension> by default
+#   end
+
+#   def index
+#     @movies = Movie.all
+#     # @all_ratings = Movies.rating
+#     if params[:ratings].nil?
+#       session[:ratings]||= @all_ratings
+#       print "++++++++no nill+++++"
+#     else 
+#       session[:ratings] =  params[:ratings].compact.values
+#       print "++++++++compact+++++"
+#     end
+#     # @movies = Movie.where(:rating => session[:ratings])
+#     # print @movies
+#     if session[:id] != nil
+#       instance_variable_set("@#{session[:id]}",'hilite')
+#     end
+#   end
+
+#   def new
+#     @movie = Movie.new
+#     # default: render 'new' template
+#   end
+
+#   def create
+#     @movie = Movie.create!(movie_params)
+#     flash[:notice] = "#{@movie.title} was successfully created."
+#     redirect_to movies_path
+#   end
+
+#   def edit
+#     @movie = Movie.find params[:id]
+#   end
+
+#   def update
+#     @movie = Movie.find params[:id]
+#     @movie.update_attributes!(movie_params)
+#     flash[:notice] = "#{@movie.title} was successfully updated."
+#     redirect_to movie_path(@movie)
+#   end
+
+#   def destroy
+#     @movie = Movie.find(params[:id])
+#     @movie.destroy
+#     flash[:notice] = "Movie '#{@movie.title}' deleted."
+#     redirect_to movies_path
+#   end
+
+# end
 class MoviesController < ApplicationController
 
   def movie_params
@@ -12,6 +71,60 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    
+    # set the all ratings variable (defined in the hw instructions)
+    @all_ratings = Movie.ratings
+    
+    # set up the sessions to keep track of
+    # which sort and which ratings are desired
+    session[:sort_by] ||= 'id'
+    session[:ratings] ||= Hash[@all_ratings.map {|rating| [rating,1]}]
+    
+    # get the ratings which were checked 
+    # :ratings is a params passed
+    # and params[:ratings] is a hash with map rating checked => 1
+    if params[:ratings].nil?
+      @ratings_desired = session[:ratings].keys
+    else 
+      @ratings_desired = params[:ratings].keys
+      # store the info for the next time
+      session[:ratings] = params[:ratings]
+    end
+    
+    # retrieve how to sort from URI route
+    if params[:sort_by].nil?
+      @sort = session[:sort_by]
+    else 
+      @sort = params[:sort_by]
+      # and keep in the session for next time
+      session[:sort_by] = params[:sort_by]
+    end
+    
+    # from hw instructions
+    # the RESTful thing to do (in case of lacking parameters from
+    # incoming URI) is to do a redirect with params
+    # filled by the session values
+    if params[:sort_by].nil? || params[:ratings].nil?
+      flash.keep
+      # see rake routes (movies path is controlled by index)
+      redirect_to movies_path(ratings: session[:ratings], sort_by: session[:sort_by])
+    end
+    
+    
+    # get the sort variable according to click
+    # in index view define session[sort] to be title (if clicked)
+    # and session[sort] = date (if clicked) 
+    if @sort == "title"
+      @title_hilite = "hilite"
+    elsif @sort == "release_date"
+      @date_hilite = "hilite"
+    end
+    
+    
+    # sort them; the list of movies in @movies will be ordered
+    # according to the sort variable
+    @movies = Movie.where(rating: @ratings_desired).order(@sort)
+    
   end
 
   def new
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,26 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  # Highlight background if necesary returning 'hilite' class name according with default.css style
+  def yellow_background(column)
+  	@ordered_by == column ? 'hilite' : ''
+  end
+
+  # helper method in order to return a hash with pair-value 'order_by: column'
+  # 'order' pretend tu switch between ascending / descending order but it is pending
+  def create_order_params(column, order=true)
+	{:order_by=> column, :asc=>true}
+  end
+
+# initialize checkboxes' rating acording to params passed in HTTP GET.
+# If not params, when is the first time the app is executed, for example, checked all boxes
+  def has_been_checked?(rating)
+  	if @checked_ratings
+  		@checked_ratings.include? rating
+  	else
+  		true
+  	end
+  end
+
 end
